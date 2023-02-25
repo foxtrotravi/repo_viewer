e8837fde2f78860a5f5d0da11e8d771fe7bb69d2
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-02-25 20:50:32 +0530

    Update main & setup navigator 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/core/presentation/app_widget.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    ProviderScope(
+      child: AppWidget(),
+    ),
+  );
 }
