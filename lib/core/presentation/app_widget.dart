90212a7e85dc2590e14828f3d23dbde1e31991b0
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-02-26 14:10:24 +0530

    Add providers for handling authentication

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,11 +1,39 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   final appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(initializationProvider);
+    ref.listen<AuthState>(authNotifierProvider, (_, authState) {
+      authState.maybeMap(
+        authenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const StarredReposRoute(),
+            predicate: (_) => false,
+          );
+        },
+        unauthenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const SignInRoute(),
+            predicate: (_) => false,
+          );
+        },
+        orElse: () {},
+      );
+    });
     return MaterialApp.router(
       title: 'Repo Viewer',
       routerDelegate: appRouter.delegate(),
