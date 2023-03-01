157c74a072219a7cee42eb5065ee35046296de47
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-03-01 15:52:11 +0530

    Add Authorization route, page & web view

---
+++ lib/auth/presentation/authorization_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class AuthorizationPage extends StatefulWidget {
+  final Uri authorizationUrl;
+  final void Function(Uri redirectUrl) onAuthorizationCodeRedirectAttempt;
+
+  const AuthorizationPage({
+    super.key,
+    required this.authorizationUrl,
+    required this.onAuthorizationCodeRedirectAttempt,
+  });
+
+  @override
+  State<AuthorizationPage> createState() => _AuthorizationPageState();
+}
+
+class _AuthorizationPageState extends State<AuthorizationPage> {
+  late final WebViewController webviewController;
+
+  @override
+  void initState() {
+    webviewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(widget.authorizationUrl)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (request) {
+            if (request.url
+                .startsWith(GithubAuthenticator.redirectUrl.toString())) {
+              widget.onAuthorizationCodeRedirectAttempt(Uri.parse(request.url));
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: webviewController,
+        ),
+      ),
+    );
+  }
+}
--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:repo_viewer/auth/presentation/authorization_page.dart';
 import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
 import 'package:repo_viewer/splash/presentation/splash_page.dart';
 import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
@@ -8,6 +9,7 @@ import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
     MaterialRoute(page: SplashPage, initial: true),
     MaterialRoute(page: SignInPage, path: '/sign-in'),
     MaterialRoute(page: StarredReposPage, path: '/starred'),
+    MaterialRoute(page: AuthorizationPage, path: '/auth'),
   ],
   replaceInRouteName: 'Page,Route',
 )
